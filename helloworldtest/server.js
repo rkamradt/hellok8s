cac806a14b6f62aa53fd694f18db36f83562917c
Randal Kamradt <randykamradt@gmail.com>
2020-03-10 14:05:11 -0700

    add logging to test

--- helloworldtest/server.js
+++ helloworldtest/server.js
@@ -5,9 +5,10 @@ const request = require('request');
 // Constants
 const URL = process.env.HELLO_URL;
 
+console.log('testing url ' + URL);
 // Main test
 request(URL, (err, res, body) => {
-  if (err) { console.log('error'); process.exit(2); }
+  if (err) { console.log('error: ' + err); process.exit(2); }
   if(body === 'Hello world\n') {
     console.log('service returned ' + body);
     process.exit(0);
